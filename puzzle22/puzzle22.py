456b79a2a5ee214136fe358be7a2fd9c42aae132
Anirudh Alameluvari <alameluvarianirudh3@gmail.com>
2023-12-11 01:38:12 -0800

    refactor

--- puzzle22/puzzle22.py
+++ puzzle22/puzzle22.py
@@ -3,30 +3,28 @@ from itertools import accumulate
 
 def solution(file, expansion):
     with open(file, "r") as f:
-        lines = f.readlines()
-        rows = [expansion - 1] * len(lines)
-        cols = [expansion - 1] * len(lines[0].strip("\n"))
+        lines = [line.strip() for line in f]
+    rows = [expansion - 1] * len(lines)
+    cols = [expansion - 1] * len(lines[0])
     galaxies = []
     for i, line in enumerate(lines):
-        for j, c in enumerate(line.strip("\n")):
+        for j, c in enumerate(line):
             if c == "#":
                 cols[j] = 0
                 rows[i] = 0
                 galaxies.append((i, j))
     rows = list(accumulate(rows))
     cols = list(accumulate(cols))
-    sum = 0
-    for i in range(len(galaxies)):
-        X, Y = galaxies[i]
-        for j in range(i + 1, len(galaxies)):
-            x, y = galaxies[j]
-            sum += (
-                abs(rows[x] - rows[X])
-                + abs(cols[y] - cols[Y])
-                + abs(x - X)
-                + abs(y - Y)
-            )
-    return sum
+
+    X, Y = zip(*[(x + rows[x], y + cols[y]) for x, y in galaxies])
+    X.sort()
+    Y.sort()
+    n = len(X)
+    return (
+        2 * sum(i * x for i, x in enumerate(X))
+        + 2 * sum(i * y for i, y in enumerate(Y))
+        - (n - 1) * (sum(X) + sum(Y))
+    )
 
 
 print(solution("input.txt", 1000000))
