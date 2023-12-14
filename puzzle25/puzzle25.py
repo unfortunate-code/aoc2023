0d11f28310ac884c6e248dffac35c00bc32f22c1
Anirudh Alameluvari <alameluvarianirudh3@gmail.com>
2023-12-13 18:22:21 -0800

    much faster approach for day 13

--- puzzle25/puzzle25.py
+++ puzzle25/puzzle25.py
@@ -2,78 +2,40 @@ def solution(file):
     with open(file) as f:
         inputs = f.read().split("\n\n")
 
-    # Finds palindrome that starts at the beginning or end and returns the largest of those.
-    def find_palindrome(arr):
-        res = -1
-        loc = -1
-        for j in range(len(arr) - 1, -1, -1):
-            if j % 2 == 0:
-                continue
-            for k in range(j // 2 + 1):
-                if arr[k] != arr[j - k]:
-                    break
-            else:
-                res = j + 1
-                loc = j // 2
-                break
-        for i in range(len(arr)):
-            if (len(arr) - i - 1) % 2 == 0:
-                continue
-            for k in range((len(arr) - i + 1) // 2):
-                if arr[i + k] != arr[len(arr) - k - 1]:
-                    break
-            else:
-                if i > res:
-                    res = len(arr) - i
-                    loc = (len(arr) + i) // 2 - 1
-                    break
-        return res, loc
+    def map_to_int(lines):
+        res = [[1 if c == "#" else 0 for c in line] for line in lines]
+        return [int("".join(map(str, line)), 2) for line in res]
 
-    # Maps the columns to integers for easy palindrome finding.
-    def vertical_map(input):
-        lines = input.split("\n")
-        cols = zip(*lines)
-        d = {}
-        vert = []
-        k = 0
-        for col in cols:
-            if col in d:
-                vert.append(d[col])
-            else:
-                d[col] = k
-                vert.append(k)
-                k += 1
-        return vert
+    def get_number_of_ones(num):
+        return bin(num).count("1")
 
-    # Maps horizontal lines to integers for easy palindrome finding.
-    def horizontal_map(input):
-        lines = input.split("\n")
-        d = {}
-        hor = []
-        k = 0
-        for line in lines:
-            if line in d:
-                hor.append(d[line])
-            else:
-                d[line] = k
-                hor.append(k)
-                k += 1
-        return hor
+    def num_smudges(top, bottom):
+        res = 0
+        for i in range(len(top)):
+            res += get_number_of_ones(top[i] ^ bottom[i])
+        return res
 
-    sum = 0
+    def find_mirror(lines):
+        r = map_to_int(lines)
+        for i in range(len(r) - 1):
+            if i < len(r) // 2:
+                top = r[: i + 1]
+                bottom = r[i + 1 : 2 * i + 2]
+            else:
+                bottom = r[i + 1 :]
+                top = r[i + 1 - len(bottom) : i + 1]
+            if num_smudges(top, bottom[::-1]) == 0:
+                return i
+        return -1
+
+    res = 0
     for input in inputs:
-        # Find mirrors on both axes and take the largest.
-        vert = vertical_map(input)
-        hor = horizontal_map(input)
-        res1, loc1 = find_palindrome(vert)
-        res2, loc2 = find_palindrome(hor)
-        if res1 > res2:
-            # 1 indexed.
-            sum += loc1 + 1
+        lines = input.splitlines()
+        if (loc := find_mirror(lines)) != -1:
+            res += 100 * (loc + 1)
         else:
-            # 1 indexed.
-            sum += (loc2 + 1) * 100
-    return sum
+            res += find_mirror(list(zip(*lines))) + 1
+    return res
 
 
 print(solution("input.txt"))
